4d920f82fc4cd8a9d206a1115676107c0057cbc2
Drace1337 <jakubrutek@wp.pl>
2025-05-09 15:29:01 +0200

    Modified 06 folder files

--- 06-organization-and-modules/consul/main.tf
+++ 06-organization-and-modules/consul/main.tf
@@ -2,9 +2,9 @@ terraform {
   # Assumes s3 bucket and dynamo DB table already set up
   # See /code/03-basics/aws-backend
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
+    bucket         = "drace-devops"
     key            = "06-organization-and-modules/consul/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-central-1"
     dynamodb_table = "terraform-state-locking"
     encrypt        = true
   }
@@ -18,7 +18,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
 
 ############################################################
@@ -31,6 +31,7 @@ provider "aws" {
 ## REPO: https://github.com/hashicorp/terraform-aws-consul
 ##
 ############################################################
-module "consul" {
-  source = "git@github.com:hashicorp/terraform-aws-consul.git"
+module "consul_cluster" {
+  source = "hashicorp/consul-oss/aws"
+  version = "0.2.0"
 }
